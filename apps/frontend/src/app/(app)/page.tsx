c56837457a2f8a15ab5bbe8c936b729d3ebbd9ff
Iqbal Pamula <balpamula@gmail.com>
2025-11-21 22:03:38 +0700

    feat(frontend): :lipstick: header

--- apps/frontend/src/app/(app)/page.tsx
+++ apps/frontend/src/app/(app)/page.tsx
@@ -1,34 +1,10 @@
-import { Button } from "@/components/ui/button";
-import { getServerSession } from "@/features/auth/api";
-import { SignOutButton } from "@/features/auth/components";
-import Link from "next/link";
-
 export default async function Home() {
-  const session = await getServerSession();
-
   return (
     <div className="h-full w-full">
       <div className="mx-auto flex h-full w-full max-w-7xl flex-col items-center justify-center border-x border-dashed p-4">
-        <div className="space-y-4 text-center">
-          <h1 className="font-mono text-6xl font-bold">
-            NestJS + Next.js Monorepo
-          </h1>
-          {session ? (
-            <SignOutButton>{session.user.name}</SignOutButton>
-          ) : (
-            <p className="text-2xl font-medium">
-              Please{" "}
-              <Button
-                asChild
-                variant="link"
-                className="p-0 text-2xl underline underline-offset-2"
-              >
-                <Link href="/sign-in">sign in</Link>
-              </Button>{" "}
-              to continue.
-            </p>
-          )}
-        </div>
+        <h1 className="text-center font-mono text-6xl font-bold">
+          Lorem ipsum dolor sit amet, consectetur adipisicing.
+        </h1>
       </div>
     </div>
   );
--- apps/frontend/src/components/layout/Header.tsx
+++ apps/frontend/src/components/layout/Header.tsx
@@ -1,10 +1,12 @@
+"use client";
+
+import { ProfileMenu } from "@/components";
 import { BookAudioIcon } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { ThemeSwitcer } from "../themes";
 
 export function Header() {
   return (
-    <div className="fixed top-0 z-[9999] h-16 w-full shrink-0 border-b border-dashed">
+    <div className="fixed top-0 z-50 h-16 w-full shrink-0 border-b border-dashed">
       <div className="mx-auto flex h-full w-full max-w-7xl flex-row items-center justify-between border-x border-dashed px-4">
         <div className="flex flex-row items-center gap-2">
           <BookAudioIcon />
@@ -12,13 +14,7 @@ export function Header() {
         </div>
         <div className="flex flex-row items-center gap-2">
           <ThemeSwitcer />
-          <Avatar>
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              alt="User Avatar"
-            />
-            <AvatarFallback>UN</AvatarFallback>
-          </Avatar>
+          <ProfileMenu />
         </div>
       </div>
     </div>
