af727c03dde00e1863ccc5f133c6b11445740760
Iqbal Pamula <140243608+balpeletsgo@users.noreply.github.com>
2025-11-21 16:34:18 +0700

    chore(backend): :card_file_box: upgrade prisma to v7 (#10)

--- apps/backend/prisma/migrations/20251121091608_init/migration.sql
+++ apps/backend/prisma/migrations/20251121091608_init/migration.sql
@@ -1,9 +1,14 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN', 'STAFF');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "isMember" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
